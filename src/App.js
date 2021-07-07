b64d9bbde0df6508607fd5f34ae4ed7f6f8ddeba
Yasi26 <yasthiru26@gmail.com>
2021-07-07 16:56:09 +0530

    Header and Footer Changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './common/Header';
+import Footer from './common/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>     
+  );
+}
+
+export default App;
---
+++ src/common/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../styles/Header.css';
+
+function Header(){
+    return(
+        <div class="toolbar">
+            <a href="" class="optus" >   
+                <img src="https://image.exacttarget.com/lib/fe9412727565007a72/m/4/Optus-logo-desktop.png" />
+            </a>  
+        </div>
+    );
+}
+
+export default Header;
