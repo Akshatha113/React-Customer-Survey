ca6a58ad9f774b0599f75282139395e55e498ec9
Akshatha B <akshatha.b@optus.com.au>
2021-07-30 12:41:33 +0530

    common folder for header and footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 // import logo from './logo.svg';
 import Welcome from './Screens/Welcome/Welcome';
-import YesNo from './DynamicComponents/YesNo/YesNo';
-import OpenEnded from './DynamicComponents/OpenEnded/OpenEnded';
-import MixedType from './DynamicComponents/MixedType/MixedType';
 
 function App() {
   return (
     <div className="App">
-      {/* <MixedType/>
-      <YesNo/>
-      <OpenEnded/> */}
       <Welcome/>
     </div>
   );
